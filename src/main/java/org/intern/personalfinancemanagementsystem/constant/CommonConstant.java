b0bbfc580c09783e683ff14d4cf32cba6ec16d42
kdieu4 <hoangthanhdieu2006@gmail.com>
2026-09-04 09:52:48 +0700

    chore(db): setup flyway and create initial users table schema

--- src/main/java/org/intern/personalfinancemanagementsystem/constant/CommonConstant.java
+++ src/main/java/org/intern/personalfinancemanagementsystem/constant/CommonConstant.java
@@ -2,7 +2,7 @@ package org.intern.personalfinancemanagementsystem.constant;
 
 public final class CommonConstant {
     public static final int FULL_NAME_MAX_LENGTH = 120;
-    public static final int EMAIL_MAX_LENGTH = 100;
+    public static final int EMAIL_MAX_LENGTH = 120;
     public static final int PASSWORD_MAX_LENGTH = 120;
     public static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[!@#$%^&*(){}_+=~`|:;'<>,./?])(?=.*[a-z])(?=.*[A-Z]).{6,120}$";
     public static final int PHONE_NUMBER_MAX_LENGTH = 15;
